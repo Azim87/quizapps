7c31eaed6bdc6d6a2dc5099dd628935d9ab3ce8e
https://github.com/Azim87 <you@example.com>
2019-11-09 02:20:03 +0600

    nice spinner and multislider added

--- app/src/main/java/com/kubatov/quizapp/ui/main/MainFragment.java
+++ app/src/main/java/com/kubatov/quizapp/ui/main/MainFragment.java
@@ -2,24 +2,35 @@ package com.kubatov.quizapp.ui.main;
 
 import android.view.View;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.kubatov.quizapp.R;
 import com.kubatov.quizapp.core.CoreFragment;
 import com.kubatov.quizapp.util.MySpinner;
 
+import org.angmarch.views.NiceSpinner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.apptik.widget.MultiSlider;
 
 
 public class MainFragment extends CoreFragment {
 
     @BindView(R.id.spinner_category)
-    Spinner spinnerCategory;
+    NiceSpinner spinnerCategory;
     @BindView(R.id.spinner_difficulty)
-    Spinner spinnerDifficulty;
+    NiceSpinner spinnerDifficulty;
+    @BindView(R.id.seek_bar_amount)
+    MultiSlider mAmountSlider;
+    @BindView(R.id.text_view_amount)
+    TextView amountTextView;
 
-    private String[] category = {"ALL"};
-    private String[] difficulty = {"ALL"};
 
     private MainViewModel mViewModel;
 
@@ -37,13 +48,24 @@ public class MainFragment extends CoreFragment {
         ButterKnife.bind(this, view);
         initCategorySpinner();
         initDifficultSpinner();
+
+
     }
 
     private void initCategorySpinner() {
-        MySpinner.show(category, spinnerCategory, getContext());
+        List<String> category = new LinkedList<>(Arrays.asList("ALL"));
+        category.add("");
+        MySpinner.show(category, spinnerCategory);
     }
 
     private void initDifficultSpinner() {
-        MySpinner.show(difficulty, spinnerDifficulty, getContext());
+        List<String> difficulty = new LinkedList<>(Arrays.asList("ALL"));
+        difficulty.add("");
+        MySpinner.show(difficulty, spinnerDifficulty);
+        getValueFromSeekBar();
+    }
+
+    private void getValueFromSeekBar() {
+        mAmountSlider.setOnThumbValueChangeListener((multiSlider, thumb, thumbIndex, value) -> amountTextView.setText(String.valueOf(value)));
     }
 }
--- app/src/main/java/com/kubatov/quizapp/util/MySpinner.java
+++ app/src/main/java/com/kubatov/quizapp/util/MySpinner.java
@@ -1,13 +1,13 @@
 package com.kubatov.quizapp.util;
 
-import android.content.Context;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import org.angmarch.views.NiceSpinner;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class MySpinner {
-    public static void show(String[] spin, Spinner spinner, Context context) {
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, spin);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        spinner.setAdapter(arrayAdapter);
+    public static void show(List<String> dataList, NiceSpinner niceSpinner) {
+        List<String> data = new LinkedList<>(dataList);
+        niceSpinner.attachDataSource(data);
     }
 }
